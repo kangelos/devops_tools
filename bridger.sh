799bd1fc298c041bf9f804eae3faf123ed6def12
Angelos Karageorgiou <angelos@unix.gr>
2023-06-09 15:30:16 +0300

    Update bridger.sh with kuttle info

--- bridger.sh
+++ bridger.sh
@@ -8,6 +8,14 @@ then
     exit 1
 fi
 
+found=$(which kuttle)
+if [ $? -ne 0 ]
+then
+    echo "Please install kuttle"
+    echo "see https://github.com/kayrus/kuttle"
+    exit 1
+fi
+
 echo grabbing Nodes IP range
 ipnodes="$(kubectl get nodes | grep -v NAME | sed -e 's/.us-[we][ea]st.*//g' | cut -f2,3 -d- | sort | uniq | sed -e 's/-/./g').0.0/16"
 echo Nodes IP range:  $ipnodes
@@ -23,7 +31,7 @@ echo DNS service: $dnshost
 echo spawning kuttle-${USER} If it errors with "already exists" it is fine
 kubectl run kuttle-${USER} --image=alpine:latest --restart=Never -- sh -c 'apk add python3 --update && exec tail -f /dev/null'
 
-#Enable this for laptops with dnsmasq ( most of them)
+#Enable this for laptops with dnsmasq (most of them)
 DNSMASQ="--dns --ns-hosts=127.0.0.1 --to-ns=$dnshost"
 # Uncomment to disable
 #DNSMASQ=""
